0ccb00bbc8fc7245ef5321f2ad1319ac3c4fcef4
Chenyang Li <chenyang.li@tum.de>
2022-06-16 17:25:08 +0200

    finish model (without test)

--- model/model.py
+++ model/model.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 
 class Model(nn.Module):
-    def __init__(self, global_feature_size: int, local_feature_size: int):
+    def __init__(self, global_feature_size=128, local_feature_size=128):
         """
         Initialise the network
 
@@ -11,7 +11,73 @@ class Model(nn.Module):
             global_feature_size (int): The length of the global feature embeddings
             local_feature_size (int): The length of the local feature embeddings
         """
-        pass
+        self.view_enc = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=7, stride=1),
+            nn.MaxPool2d(kernel_size=2),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Conv2d(in_channels=96, out_channels=128, kernel_size=3, stride=1),
+            nn.MaxPool2d(kernel_size=2),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1),
+            nn.MaxPool2d(kernel_size=2),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1),
+            nn.MaxPool2d(kernel_size=2),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1),
+            nn.MaxPool2d(kernel_size=2),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1),
+            nn.MaxPool2d(kernel_size=2),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Linear(in_features=268, out_features=global_feature_size),
+            nn.ReLU(),
+        )
+        self.class_enc = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=7, stride=1),
+            nn.MaxPool2d(kernel_size=2),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Conv2d(in_channels=96, out_channels=128, kernel_size=3, stride=1),
+            nn.MaxPool2d(kernel_size=2),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1),
+            nn.MaxPool2d(kernel_size=2),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1),
+            nn.MaxPool2d(kernel_size=2),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1),
+            nn.MaxPool2d(kernel_size=2),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1),
+            nn.MaxPool2d(kernel_size=2),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Linear(in_features=268, out_features=local_feature_size),
+            nn.ReLU(),
+        )
+        self.class_dec = nn.Sequential(  # might need to be modified
+            nn.Linear(in_features=global_feature_size, out_features=64),
+            nn.ReLU(),
+            nn.Linear(in_features=64, out_features=32),
+            nn.ReLU(),
+            nn.Linear(in_features=32, out_features=1),
+            nn.ReLU(),
+        )
+        self.shape_dec = nn.Sequential(
+            nn.Linear(
+                in_features=global_feature_size + local_feature_size, out_features=8192
+            ),
+            nn.ReLU(),
+            nn.ConvTranspose3d(
+                in_channels=8192, out_channels=128, kernel_size=5, stride=2
+            ),
+            nn.ConvTranspose3d(
+                in_channels=128, out_channels=64, kernel_size=5, stride=2
+            ),
+            nn.ConvTranspose3d(
+                in_channels=128, out_channels=1, kernel_size=5, stride=2
+            ),
+        )
 
     def forward(self, x_in_class: torch.Tensor, x_in_3d: torch.Tensor):
         """
@@ -26,5 +92,8 @@ class Model(nn.Module):
             pred_3d (torch.Tensor): (B, 32, 32, 32) tensor. Reconstructed voxels.
         """
 
-        pass
-        # return pred_class, pred_3d
+        class_emb = self.class_enc(x_in_class)
+        view_emb = self.view_enc(x_in_3d)
+        pred_class = self.class_dec(class_emb)
+        pred_3d = self.shape_dec(torch.cat((class_emb, view_emb), dim=0))
+        return pred_class, pred_3d
