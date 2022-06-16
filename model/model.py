fdbec37848e27d1e32c08e4baa88390d74aa63fb
Chenyang Li <chenyang.li@tum.de>
2022-06-16 22:32:29 +0200

    finish model (with diff. ConvTranspose3d)

--- model/model.py
+++ model/model.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 
 class Model(nn.Module):
-    def __init__(self, global_feature_size=128, local_feature_size=128):
+    def __init__(self, global_feature_size=128, local_feature_size=128, num_class=13):
         """
         Initialise the network
 
@@ -11,72 +11,71 @@ class Model(nn.Module):
             global_feature_size (int): The length of the global feature embeddings
             local_feature_size (int): The length of the local feature embeddings
         """
+        super(Model, self).__init__()
         self.view_enc = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=7, stride=1),
+            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=7),
             nn.MaxPool2d(kernel_size=2),
             nn.LeakyReLU(negative_slope=0.01),
-            nn.Conv2d(in_channels=96, out_channels=128, kernel_size=3, stride=1),
+            nn.Conv2d(in_channels=96, out_channels=128, kernel_size=3),
             nn.MaxPool2d(kernel_size=2),
             nn.LeakyReLU(negative_slope=0.01),
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1),
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3),
             nn.MaxPool2d(kernel_size=2),
             nn.LeakyReLU(negative_slope=0.01),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3),
             nn.MaxPool2d(kernel_size=2),
             nn.LeakyReLU(negative_slope=0.01),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3),
             nn.MaxPool2d(kernel_size=2),
             nn.LeakyReLU(negative_slope=0.01),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1),
-            nn.MaxPool2d(kernel_size=2),
-            nn.LeakyReLU(negative_slope=0.01),
-            nn.Linear(in_features=268, out_features=global_feature_size),
+            View((-1, 256)),
+            nn.Linear(in_features=256, out_features=local_feature_size),
             nn.ReLU(),
         )
+
         self.class_enc = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=7, stride=1),
+            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=7),
             nn.MaxPool2d(kernel_size=2),
             nn.LeakyReLU(negative_slope=0.01),
-            nn.Conv2d(in_channels=96, out_channels=128, kernel_size=3, stride=1),
+            nn.Conv2d(in_channels=96, out_channels=128, kernel_size=3),
             nn.MaxPool2d(kernel_size=2),
             nn.LeakyReLU(negative_slope=0.01),
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1),
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3),
             nn.MaxPool2d(kernel_size=2),
             nn.LeakyReLU(negative_slope=0.01),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3),
             nn.MaxPool2d(kernel_size=2),
             nn.LeakyReLU(negative_slope=0.01),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3),
             nn.MaxPool2d(kernel_size=2),
-            nn.LeakyReLU(negative_slope=0.01),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1),
-            nn.MaxPool2d(kernel_size=2),
-            nn.LeakyReLU(negative_slope=0.01),
-            nn.Linear(in_features=268, out_features=local_feature_size),
+            View((-1, 256)),
+            nn.Linear(in_features=256, out_features=local_feature_size),
             nn.ReLU(),
         )
+
         self.class_dec = nn.Sequential(  # might need to be modified
             nn.Linear(in_features=global_feature_size, out_features=64),
             nn.ReLU(),
             nn.Linear(in_features=64, out_features=32),
             nn.ReLU(),
-            nn.Linear(in_features=32, out_features=1),
+            nn.Linear(in_features=32, out_features=num_class),
             nn.ReLU(),
         )
+
         self.shape_dec = nn.Sequential(
             nn.Linear(
                 in_features=global_feature_size + local_feature_size, out_features=8192
             ),
             nn.ReLU(),
+            View((-1, 128, 4, 4, 4)),
             nn.ConvTranspose3d(
-                in_channels=8192, out_channels=128, kernel_size=5, stride=2
+                in_channels=128, out_channels=128, kernel_size=1, stride=2
             ),
             nn.ConvTranspose3d(
-                in_channels=128, out_channels=64, kernel_size=5, stride=2
+                in_channels=128, out_channels=64, kernel_size=3, stride=2
             ),
-            nn.ConvTranspose3d(
-                in_channels=128, out_channels=1, kernel_size=5, stride=2
-            ),
+            nn.ConvTranspose3d(in_channels=64, out_channels=1, kernel_size=4, stride=2),
+            nn.Sigmoid(),
         )
 
     def forward(self, x_in_class: torch.Tensor, x_in_3d: torch.Tensor):
@@ -88,12 +87,21 @@ class Model(nn.Module):
             x_in_3d (torch.Tensor): (B, 3, 127, 127) tensor. Input for extracting local features.
 
         Returns:
-            pred_class (torch.Tensor): (B,) tensor. Classification results.
+            pred_class (torch.Tensor): (B, num_class) tensor. Classification results.
             pred_3d (torch.Tensor): (B, 32, 32, 32) tensor. Reconstructed voxels.
         """
 
         class_emb = self.class_enc(x_in_class)
         view_emb = self.view_enc(x_in_3d)
         pred_class = self.class_dec(class_emb)
-        pred_3d = self.shape_dec(torch.cat((class_emb, view_emb), dim=0))
+        pred_3d = self.shape_dec(torch.cat((class_emb, view_emb), dim=1)).squeeze(1)
         return pred_class, pred_3d
+
+
+class View(nn.Module):
+    def __init__(self, shape):
+        super(View, self).__init__()
+        self.shape = shape
+
+    def forward(self, x):
+        return x.view(*self.shape)
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 # %%
-
+# test dataset
 from data.shapenet import ShapeNetDataset
 from data.shapenet_loader import ShapeNetDataLoader
 
@@ -9,8 +9,7 @@ dataset = ShapeNetDataset(split, val_view=3, test_view=3)
 item1 = dataset[0]
 item2 = dataset[1]
 # %%
-# %%
-
+# test trainloader
 trainloader = ShapeNetDataLoader(dataset, batch_size=3, shape_num=3)
 
 i = 0
@@ -20,4 +19,15 @@ for batch in trainloader:
 
     pdb.set_trace()
     print(type(batch))
+import torch
+
+# %%
+# test model
+from model.model import Model
+
+inputs = torch.rand((5, 3, 127, 127))
+output_class, output_3d = Model()(inputs, inputs)
+assert output_class.shape == (5, 13)
+assert output_3d.shape == (5, 32, 32, 32)
+
 # %%
