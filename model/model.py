6ba30fbd0194e92c7f7f7a31ec321d5d836d5c2f
Chenyang Li <chenyang.li@tum.de>
2022-07-17 21:18:02 +0200

    finish model

--- model/model.py
+++ model/model.py
@@ -1,10 +1,12 @@
+from math import ceil
+
 import torch
 import torch.nn as nn
-import torchvision
+import torch.nn.functional as F
 
 
 class Model(nn.Module):
-    def __init__(self, global_feature_size=128, local_feature_size=128, num_class=13):
+    def __init__(self):
         """
         Initialise the network
 
@@ -13,66 +15,76 @@ class Model(nn.Module):
             local_feature_size (int): The length of the local feature embeddings
         """
         super(Model, self).__init__()
-        self.view_enc = nn.Sequential(
+        self.vox_enc = nn.Sequential(
+            Conv3dSame(in_channels=1, out_channels=32, kernel_size=5, stride=2),
+            nn.LeakyReLU(negative_slope=0.3),
+            nn.BatchNorm3d(32),
+            Conv3dSame(in_channels=32, out_channels=64, kernel_size=5, stride=2),
+            nn.LeakyReLU(negative_slope=0.3),
+            nn.BatchNorm3d(64),
+            Conv3dSame(in_channels=64, out_channels=128, kernel_size=5, stride=2),
+            nn.LeakyReLU(negative_slope=0.3),
+            nn.BatchNorm3d(128),
+            View((-1, 128 * 4 * 4 * 4)),
+            nn.Linear(in_features=128 * 4 * 4 * 4, out_features=512),
+            nn.ReLU(),
+            nn.Linear(in_features=512, out_features=128),
+        )
+        self.image_enc = nn.Sequential(
             nn.Conv2d(in_channels=3, out_channels=96, kernel_size=7),
             nn.MaxPool2d(kernel_size=2),
             nn.LeakyReLU(negative_slope=0.01),
+            nn.BatchNorm2d(96),
             nn.Conv2d(in_channels=96, out_channels=128, kernel_size=3),
             nn.MaxPool2d(kernel_size=2),
             nn.LeakyReLU(negative_slope=0.01),
+            nn.BatchNorm2d(128),
             nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3),
             nn.MaxPool2d(kernel_size=2),
             nn.LeakyReLU(negative_slope=0.01),
+            nn.BatchNorm2d(256),
             nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3),
             nn.MaxPool2d(kernel_size=2),
             nn.LeakyReLU(negative_slope=0.01),
+            nn.BatchNorm2d(256),
             nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3),
             nn.MaxPool2d(kernel_size=2),
             nn.LeakyReLU(negative_slope=0.01),
+            nn.BatchNorm2d(256),
             View((-1, 256)),
-            nn.Linear(in_features=256, out_features=local_feature_size),
-            nn.ReLU(),
-        )
-
-        self.class_enc = torchvision.models.resnet18(pretrained=True)
-        self.class_enc.fc = nn.Linear(in_features=512, out_features=global_feature_size)
-
-        self.class_dec = nn.Sequential(  # might need to be modified
-            nn.Linear(in_features=global_feature_size, out_features=64),
-            nn.ReLU(),
-            nn.Linear(in_features=64, out_features=32),
-            nn.ReLU(),
-            nn.Linear(in_features=32, out_features=num_class),
+            nn.Linear(in_features=256, out_features=128),
             nn.ReLU(),
         )
 
         self.shape_dec = nn.Sequential(
-            nn.Linear(
-                in_features=global_feature_size + local_feature_size, out_features=8192
-            ),
+            nn.Linear(in_features=128, out_features=8192),
             nn.ReLU(),
-            View((-1, 8192, 1, 1, 1)),
-            nn.ConvTranspose3d(
-                in_channels=8192, out_channels=128, kernel_size=5, stride=2
+            View((-1, 128, 4, 4, 4)),
+            ConvTranspose3dSame(
+                in_channels=128,
+                out_channels=128,
+                kernel_size=5,
+                stride=2,
             ),
-            nn.ConvTranspose3d(
+            nn.BatchNorm3d(128),
+            ConvTranspose3dSame(
                 in_channels=128,
                 out_channels=64,
                 kernel_size=5,
                 stride=2,
-                output_padding=1,
             ),
-            nn.ConvTranspose3d(
+            nn.BatchNorm3d(64),
+            ConvTranspose3dSame(
                 in_channels=64,
                 out_channels=1,
                 kernel_size=5,
                 stride=2,
-                output_padding=1,
             ),
+            nn.BatchNorm3d(1),
             nn.Sigmoid(),
         )
 
-    def forward(self, x_in_class: torch.Tensor, x_in_3d: torch.Tensor):
+    def forward(self, prior: torch.Tensor, image: torch.Tensor):
         """
         Prediction for the input
 
@@ -85,11 +97,10 @@ class Model(nn.Module):
             pred_3d (torch.Tensor): (B, 32, 32, 32) tensor. Reconstructed voxels.
         """
 
-        class_emb = self.class_enc(x_in_class)
-        view_emb = self.view_enc(x_in_3d)
-        pred_class = self.class_dec(class_emb)
-        pred_3d = self.shape_dec(torch.cat((class_emb, view_emb), dim=1)).squeeze(1)
-        return pred_class, pred_3d
+        vox_emb = self.vox_enc(prior)
+        image_emb = self.image_enc(image)
+        pred_3d = self.shape_dec(vox_emb + image_emb).squeeze(1)
+        return pred_3d
 
 
 class View(nn.Module):
@@ -99,3 +110,83 @@ class View(nn.Module):
 
     def forward(self, x):
         return x.view(*self.shape)
+
+
+class Conv3dSame(torch.nn.Conv3d):
+    def calc_same_pad(self, i: int, k: int, s: int, d: int) -> int:
+        return max((ceil(i / s) - 1) * s + (k - 1) * d + 1 - i, 0)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        i0, i1, i2 = x.size()[-3:]
+
+        pad_0 = self.calc_same_pad(
+            i=i0, k=self.kernel_size[0], s=self.stride[0], d=self.dilation[0]
+        )
+        pad_1 = self.calc_same_pad(
+            i=i1, k=self.kernel_size[1], s=self.stride[1], d=self.dilation[1]
+        )
+        pad_2 = self.calc_same_pad(
+            i=i2, k=self.kernel_size[2], s=self.stride[2], d=self.dilation[2]
+        )
+
+        if pad_0 > 0 or pad_1 > 0 or pad_2 > 0:
+            x = F.pad(
+                x,
+                [
+                    pad_2 // 2,
+                    pad_2 - pad_2 // 2,
+                    pad_1 // 2,
+                    pad_1 - pad_1 // 2,
+                    pad_0 // 2,
+                    pad_0 - pad_0 // 2,
+                ],
+            )
+        return F.conv3d(
+            x,
+            self.weight,
+            self.bias,
+            self.stride,
+            self.padding,
+            self.dilation,
+            self.groups,
+        )
+
+
+class ConvTranspose3dSame(torch.nn.ConvTranspose3d):
+    def calc_same_pad(self, i: int, k: int, s: int, d: int) -> int:
+        return max((i - 1) * s + d * k + 1 - i * s, 0)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        i0, i1, i2 = x.size()[-3:]
+
+        pad_0 = self.calc_same_pad(
+            i=i0, k=self.kernel_size[0], s=self.stride[0], d=self.dilation[0]
+        )
+        pad_1 = self.calc_same_pad(
+            i=i1, k=self.kernel_size[1], s=self.stride[1], d=self.dilation[1]
+        )
+        pad_2 = self.calc_same_pad(
+            i=i2, k=self.kernel_size[2], s=self.stride[2], d=self.dilation[2]
+        )
+        x = F.conv_transpose3d(
+            x,
+            self.weight,
+            self.bias,
+            self.stride,
+            self.padding,
+            self.dilation,
+            self.groups,
+        )
+        if pad_0 > 0 or pad_1 > 0 or pad_2 > 0:
+            x = F.pad(
+                x,
+                [
+                    -(pad_2 // 2),
+                    -pad_2 + pad_2 // 2,
+                    -(pad_1 // 2),
+                    -pad_1 + pad_1 // 2,
+                    -(pad_0 // 2),
+                    -pad_0 + pad_0 // 2,
+                ],
+            )
+        return x
--- test.py
+++ test.py
@@ -1,136 +1,15 @@
 # %%
-# import here
 import torch
+from torch.utils.data import DataLoader
 
 from data.shapenet import ShapeNetDataset
-from data.shapenet_loader import ShapeNetDataLoader
-from model import train
 from model.model import Model
-from util.visualization import visualize_2d, visualize_3d
 
-# %%
-# test trainloader
-split = "view_val"
-dataset = ShapeNetDataset(split, val_view=3, test_view=3)
-trainloader = ShapeNetDataLoader(dataset, batch_size=3, shape_num=3)
-
-i = 0
-for batch in trainloader:
-    i += 1
-    # import pdb
-    print(batch.keys())
-    # pdb.set_trace()
-
-
-# %%
-# test model
-inputs = torch.rand((5, 3, 127, 127))
-output_class, output_3d = Model()(inputs, inputs)
-assert output_class.shape == (5, 13)
-assert output_3d.shape == (5, 32, 32, 32)
-
-
-# %%
-# test train
-config = {
-    "experiment_name": "test",
-    "device": "cpu",  # or 'cuda:0'
-    "batch_size": 10,
-    "resume_ckpt": None,
-    "learning_rate": 0.001,
-    "max_epochs": 1,
-    "print_every_n": 1,
-    "validate_every_n": 1,
-    "val_view": 3,
-    "test_view": 3,
-    "a": 1,
-    "b": 1,
-    "global_feature_size": 128,
-    "local_feature_size": 128,
-    "num_class": 13,
-}
-
-train.main(config)
-
-# %%
-# test visualization
-split = "view_val"
-dataset = ShapeNetDataset(split, val_view=3, test_view=3)
-trainloader = ShapeNetDataLoader(dataset, batch_size=3, shape_num=3)
-batch = trainloader.__iter__().__next__()
-visualize_3d(batch["3D"][0], batch["3D"][0], "test")
-visualize_2d(batch["class"][0])
-# %%
-# test test
-config = {
-    "experiment_name": "test",
-    "device": "cpu",  # or 'cuda:0'
-    "batch_size": 10,
-    "resume_ckpt": None,
-    "learning_rate": 0.001,
-    "max_epochs": 1,
-    "print_every_n": 1,
-    "validate_every_n": 1,
-    "val_view": 3,
-    "test_view": 3,
-    "a": 1,
-    "b": 1,
-    "global_feature_size": 128,
-    "local_feature_size": 128,
-    "num_class": 13,
-}
-test_dataset_view = ShapeNetDataset(
-    "view_test", config["val_view"], config["test_view"]
-)
-test_dataloader_view = ShapeNetDataLoader(
-    test_dataset_view[
-        :10
-    ],  # Datasets return data one sample at a time; Dataloaders use them and aggregate samples into batches
-    batch_size=config["batch_size"],  # The size of batches is defined here
-)
-
-test_dataset_shape = ShapeNetDataset(
-    "shape_test", config["val_view"], config["test_view"]
-)
-test_dataloader_shape = ShapeNetDataLoader(
-    test_dataset_shape[
-        :10
-    ],  # Datasets return data one sample at a time; Dataloaders use them and aggregate samples into batches
-    batch_size=config["batch_size"],  # The size of batches is defined here
-)
-model = Model(
-    config["global_feature_size"], config["local_feature_size"], config["num_class"]
-)
-model.load_state_dict(
-    torch.load(
-        f'./runs/{config["experiment_name"]}/model_best.ckpt',
-        map_location="cpu",
-    )
-)
-train.test(
-    model,
-    test_dataloader_view,
-    test_dataloader_shape,
-    config,
-)
-# %%
-# test k prior
-from data.shapenet import ShapeNetDataset
-from util.visualization import visualize_2d, visualize_3d
-
-split = "view_val"
-dataset = ShapeNetDataset(split, val_view=3, test_view=3)
-
-k_prior = dataset.get_k_prior_by_category(category=0, k=1)
-visualize_3d(k_prior, k_prior, "k-prior")
-
-# %%
-# test full prior
-from data.shapenet import ShapeNetDataset
-from util.visualization import visualize_2d, visualize_3d
-
-split = "view_val"
-dataset = ShapeNetDataset(split, val_view=3, test_view=3)
-full_prior = dataset.get_full_prior()
-assert full_prior.shape == (13, 3, 32, 32, 32)
+dataset = ShapeNetDataset("train", prior_k=5)
+data_loader = DataLoader(dataset, batch_size=10, shuffle=True)
+model = Model()
+for batch in data_loader:
+    pred = model(batch["3D_prior"], batch["Image"])
+    print(pred.shape)
+    break
 # %%
